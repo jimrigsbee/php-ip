0fa894450ef64450c22f3bde848f5055d40d819b
Jim Rigsbee <jrigsbee@redhat.com>
2019-04-10 10:55:46 -0400

    fancier function

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+function getUserIpAddr(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //ip pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
 print "Hello. The invoker IP address is " . $_SERVER['REMOTE_ADDR'] . "\n";
-print "Hello. The Real IP address is " . $_SERVER['HTTP_CLIENT_IP'] . "\n";
+print "Hello. The Real IP address is " . getUserIpAddr() . "\n";
 ?>
